f4d2273879b8527a5b272ae63dea808e59e65236
Erick Solis <ericksolisz@outlook.com>
2025-05-05 19:03:25 -0600

    obtenerDatosGraficos method to requests.js

--- Cryptato-Server/client/hooks/request.ts
+++ Cryptato-Server/client/hooks/request.ts
@@ -34,4 +34,35 @@ const obtenerUsuario = async (symbol: string) => {
   }
 };
 
-export default obtenerUsuario;
+const obtenerDatosGraficos = async (symbol: string, limit: number) => {
+  
+  try {
+    const response = await fetch(`${apiUrl}/data/graphicdata`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ symbol, limit }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const graphicData = await response.json();
+    console.log('Datos de gráfica recibidos:', graphicData);
+
+    // Verifica que sea un array de arrays con al menos un dato
+    if (Array.isArray(graphicData) && graphicData.length > 0 && Array.isArray(graphicData[0])) {
+      return graphicData; // Devuelve los datos crudos para graficar
+    } else {
+      throw new Error('La respuesta no tiene el formato esperado');
+    }
+  } catch (error) {
+    console.error('Error al obtener datos de gráfica:', error);
+    throw error;
+  }
+};
+
+
+exports={obtenerUsuario, obtenerDatosGraficos} ;
